13dc8dfb7a9d90bd32056fb8924be0e7e4433700
peterxia97 <peter.xia97@gmail.com>
2026-03-28 22:02:44 +1000

    fix: correct App component filename for Vercel build

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './styles.css'
+import App from './app'
+import './style.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <app />
 )
 ``
